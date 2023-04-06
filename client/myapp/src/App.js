4574271cb77e0b748dd61a9b9d2f786f4c7a4697
nromboug <noahrombough@gmail.com>
2023-04-05 20:35:49 -0400

    header items added

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <h1>
+                        Campus Buddy
+                    </h1>
+                    <div>
+                        <Link className='nav-link' to='/'>
+                            My Dashboard
+                        </Link>
+                        <Link className='nav-link' to='/'>
+                            My Dashboard
+                        </Link>
+                    </div>
+                </header>
+                <body>
+                    This body
+                </body>
+            </div>
+        </Router>
+
+    );
 }
 
 export default App;
