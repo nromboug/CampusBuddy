670d906e1c2dd61cd529a7a4b690e63a81945805
nromboug <noahrombough@gmail.com>
2023-04-24 22:13:04 -0400

    beginning of redis sessions

--- server/app.js
+++ server/app.js
@@ -2,7 +2,36 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const configRoutes = require('./routes/index.js');
+const redis = require('redis');
+const RedisStore = require('connect-redis').default;
 const cors = require('cors');
+
+const redisClient = redis.createClient({
+    host: 'localhost',
+    port: 6379
+})
+
+redisClient.on('error', function (err) {
+    console.log('Could not establish a connection with redis. ' + err);
+});
+redisClient.on('connect', function (err) {
+    console.log('Connected to redis successfully');
+});
+
+redisClient.connect()
+
+let redisStore = new RedisStore({
+    client: redisClient
+})
+
+app.use(
+    session({
+        store: redisStore,
+        resave: false, // required: force lightweight session keep alive (touch)
+        saveUninitialized: false, // recommended: only save session when data exists
+        secret: "secret phrase",
+    })
+)
 
 app.use(cors());
 
