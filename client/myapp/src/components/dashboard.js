96b31db43dd34839c81ed6e6087fd476934b06ea
nromboug <noahrombough@gmail.com>
2023-05-07 11:23:19 -0400

    syntax

--- client/myapp/src/components/dashboard.js
+++ client/myapp/src/components/dashboard.js
@@ -8,7 +8,7 @@ export default function Dashboard(props) {
     const currentUser = props.user; 
     return(
         <div style={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'space-evenly', marginTop: "25px"}}>
-            <Todo user={currentUser}}/>
+            <Todo user={currentUser} />
             <Schedule user={currentUser}/>
             <Goals user={currentUser}/>
         </div>
