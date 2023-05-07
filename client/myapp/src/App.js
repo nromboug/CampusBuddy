4aa3d27936460c74f5c9530af8c037bef89cb8e1
nromboug <noahrombough@gmail.com>
2023-05-07 11:19:18 -0400

    bug fix in todo reconciliation with db

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -16,19 +16,31 @@ import AuthComponent from './components/AuthComponent';
 import Sessions from './components/sessions';
 
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 function App() {
     const [userInfo, setUserInfo] = useState(undefined);
+
+    useEffect((e) => {
+        const logout = async () => {
+            try {
+                await axios.get('http://localhost:3001/users/logout');
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        logout();
+    }, [])
     return (
         <Router>
             <div className="App">
                 <header className="App-header">
                     <div className='title-bar'>
                         <div className='prof-stuff'>
-                            <Avatar className='avatar' src={userInfo && userInfo.image ? userInfo.image : noPfp} alt='profile picture'/>
-                            <BasicMenu user={userInfo} setUserInfo={setUserInfo}/>
+                            <Avatar className='avatar' src={userInfo && userInfo.image ? userInfo.image : noPfp} alt='profile picture' />
+                            <BasicMenu user={userInfo} setUserInfo={setUserInfo} />
                         </div>
 
 
@@ -59,13 +71,13 @@ function App() {
                 <br />
                 <div className="App-body">
                     <Routes>
-                        <Route exact path='/home' element={<Home user={userInfo}/>} />
-                        <Route exact path='/signup' element={<Signup user={userInfo}/>} />
-                        <Route exact path='/login' element={<Login user={userInfo} setUserInfo={setUserInfo}/>} />
-                        <Route path='/' element={<AuthComponent user={userInfo}/>}>
-                            <Route path='/dashboard' element={<Dashboard user={userInfo}/>} />
-                            <Route path='/profile' element={<Profile user={userInfo}/>} />
-                            <Route path='/sessions' element={<Sessions user={userInfo}/>} />
+                        <Route exact path='/home' element={<Home user={userInfo} />} />
+                        <Route exact path='/signup' element={<Signup user={userInfo} />} />
+                        <Route exact path='/login' element={<Login user={userInfo} setUserInfo={setUserInfo} />} />
+                        <Route path='/' element={<AuthComponent user={userInfo} />}>
+                            <Route path='/dashboard' element={<Dashboard user={userInfo} />} />
+                            <Route path='/profile' element={<Profile user={userInfo} />} />
+                            <Route path='/sessions' element={<Sessions user={userInfo} />} />
                         </Route>
                     </Routes>
                 </div>
--- client/myapp/src/index.js
+++ client/myapp/src/index.js
@@ -4,16 +4,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
