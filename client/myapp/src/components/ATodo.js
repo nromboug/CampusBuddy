9476802a6de17e81c78553c9be3e8ac9ef51fb8a
cindytran211 <Cindy795Tran@gmail.com>
2023-05-08 14:35:37 -0400

    fixed todo toggle i hope

--- client/myapp/src/components/ATodo.js
+++ client/myapp/src/components/ATodo.js
@@ -3,35 +3,22 @@ import actions from '../actions';
 import { List, ListItem, ListItemIcon, ListItemButton, Checkbox, ListItemText, FormControlLabel, Button, Card, CardContent, Box, Typography } from '@mui/material';
 import axios from 'axios';
 import { useState } from 'react';
-
 function Todo(props) {
-    //const dispatch = useDispatch();
-    const [completed, setCompleted] = useState(props.todo.completed);
-    const checkTodo = async () => {
-        console.log("To-Do");
-        console.log(props.todo.id);
-        // dispatch(actions.checkUncheckTodo(props.user._id,props.todo.id))
-        try {
-            const res = await axios.patch('http://localhost:3001/todos/', {
-                id: props.todo.id
-            })
-            console.log(res);
-            setCompleted(!completed)
-        } catch (err) {
-            console.log(err);
-        }
-
-    }
+    const dispatch = useDispatch();
+   const checkUncheck=()=>{
+    dispatch(actions.checkUncheckTodo(props.user._id,props.todo.id));
+   }
     return (
         <ListItem key={props.todo.id}>
-            <ListItemButton role={undefined} onClick={checkTodo} dense>
+            <ListItemButton role={undefined} dense>
                 <ListItemIcon>
                 <FormControlLabel
                     control={<Checkbox
                         edge="start"
-                        checked={completed}
+                        checked={props.todo.completed}
                         tabIndex={-1}
                         disableRipple
+                        onClick={checkUncheck}
                     />}
                     label={props.todo.todo}
                     style={{ color: 'black' }}/>
