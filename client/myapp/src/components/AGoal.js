9acc8fec3bafa5425eb8942dace1476da964121b
nromboug <noahrombough@gmail.com>
2023-05-08 15:45:49 -0400

    fixed async issues between redux/backend

--- client/myapp/src/components/AGoal.js
+++ client/myapp/src/components/AGoal.js
@@ -1,58 +1,62 @@
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import actions from '../actions';
-import { Card, CardContent, Box, Grid, CardHeader, Button, IconButton, Typography, LinearProgress }  from '@mui/material';
+import { Card, CardContent, Box, Grid, CardHeader, Button, IconButton, Typography, LinearProgress } from '@mui/material';
 import { AddCircle, RemoveCircle } from '@mui/icons-material';
+import axios from 'axios';
 
 function Goal(props) {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const incrementGoal=()=>{
-    dispatch(actions.incrementGoal(props.user._id,props.goal.id));
-  }
+    const incrementGoal = async () => {
+        await axios.patch("http://localhost:3001/goals/increment",{userId: props.user._id,id:props.goal.id});
+        dispatch(actions.incrementGoal(props.user._id, props.goal.id));
+    }
 
-  const decrementGoal=()=>{
-    dispatch(actions.decrementGoal(props.user._id,props.goal.id));
-  }
+    const decrementGoal = async () => {
+        await axios.patch("http://localhost:3001/goals/decrement",{userId: props.user._id,id:props.goal.id});
+        dispatch(actions.decrementGoal(props.user._id, props.goal.id));
+    }
 
-  const deleteGoal = () => {
-    dispatch(actions.deleteGoal(props.user._id,props.goal.id));
-  };
+    const deleteGoal = async () => {
+        await axios.delete(`http://localhost:3001/goals/${props.user._id}/${props.goal.id}`);
+        dispatch(actions.deleteGoal(props.user._id, props.goal.id));
+    };
 
-  return (
-<Card key={props.goal.id} sx={{ maxWidth: 400, margin: 'auto', marginTop: 2 }}>
-    <CardContent>
-        <Typography>
-            {props.goal.goal} 
-        </Typography>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-                <IconButton
-                    color="primary"
-                    disabled={props.goal.progress === 0}
-                    onClick={()=>decrementGoal()}
-                  >
-                <RemoveCircle />
-                </IconButton>
-                <Typography variant="body1">
-                    Target: {props.goal.progress} / {props.goal.target} 
+    return (
+        <Card key={props.goal.id} sx={{ maxWidth: 400, margin: 'auto', marginTop: 2 }}>
+            <CardContent>
+                <Typography>
+                    {props.goal.goal}
                 </Typography>
-                <IconButton
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <IconButton
+                        color="primary"
+                        disabled={props.goal.progress === 0}
+                        onClick={() => decrementGoal()}
+                    >
+                        <RemoveCircle />
+                    </IconButton>
+                    <Typography variant="body1">
+                        Target: {props.goal.progress} / {props.goal.target}
+                    </Typography>
+                    <IconButton
+                        color="primary"
+                        disabled={props.goal.progress >= props.goal.target}
+                        onClick={() => incrementGoal()}
+                    >
+                        <AddCircle />
+                    </IconButton>
+                </div>
+                <LinearProgress
+                    variant="determinate"
+                    value={(props.goal.progress / props.goal.target) * 100}
                     color="primary"
-                    disabled={props.goal.progress >= props.goal.target}
-                    onClick={()=>incrementGoal()}
-                  >
-                <AddCircle />
-                  </IconButton>
-            </div>
-            <LinearProgress
-                variant="determinate"
-                value={(props.goal.progress / props.goal.target) * 100}
-                color="primary"
-                sx={{ width: "100%", height: 8 }}
-            />
-            <Button onClick={deleteGoal}>Delete Goal</Button>
-    </CardContent>
-</Card>
-);
+                    sx={{ width: "100%", height: 8 }}
+                />
+                <Button onClick={deleteGoal}>Delete Goal</Button>
+            </CardContent>
+        </Card>
+    );
 }
 
 export default Goal;
