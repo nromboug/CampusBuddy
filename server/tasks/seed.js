33a81ad19d5e0768beedfa78979ea277721b26e4
nromboug <noahrombough@gmail.com>
2023-05-09 14:10:04 -0400

    new seed users

--- server/tasks/seed.js
+++ server/tasks/seed.js
@@ -1,22 +1,81 @@
 const connection = require('../config/mongoConnection');
 //const {sessions, users} = require('../config/mongoCollections');
+const mongoCollections = require("../config/mongoCollections");
+const users = mongoCollections.users;
 const data = require("../data");
-const users = data.users;
 const sessions = data.sessions;
 const main = async () => {
-  const db = await connection.connectToDb();
-  await db.dropDatabase();
+    const db = await connection.connectToDb();
+    await db.dropDatabase();
 
-  const s1 = await sessions.createSession("Private Event",new Date("2023-04-10T10:00:00Z"),new Date("2023-04-15T10:00:00Z"),true,"user123","something123");
-  const s2 = await sessions.createSession("Public Event",new Date("2023-04-10T10:00:00Z"),new Date("2023-04-15T10:00:00Z"),false,"user123",null);
-  const s3 = await sessions.createSession("Week Long Event",new Date("2023-05-05T10:00:00Z"),new Date("2023-05-12T10:00:00Z"),false,"user123",null);
-  const s4 = await sessions.createSession("Big Event",new Date("2023-05-05T10:00:00Z"),new Date("2023-05-12T10:00:00Z"),false,"user123",null);
-  await sessions.updateSession(s4._id.toString(), {guests: ["user123", "guest 1", "guest 2", "guest 3"]});
+    const s1 = await sessions.createSession("Private Event", new Date("2023-04-10T10:00:00Z"), new Date("2023-04-15T10:00:00Z"), true, "dummy1", "something123");
+    const s2 = await sessions.createSession("Public Event", new Date("2023-04-10T10:00:00Z"), new Date("2023-04-15T10:00:00Z"), false, "dummy2", null);
+    const s3 = await sessions.createSession("Week Long Event", new Date("2023-05-05T10:00:00Z"), new Date("2023-05-12T10:00:00Z"), false, "dummy2", null);
+    const s4 = await sessions.createSession("Big Event", new Date("2023-05-05T10:00:00Z"), new Date("2023-05-12T10:00:00Z"), false, "dummy3", null);
+    await sessions.updateSession(s4._id.toString(), { guests: ["dummy1"] });
 
-  // initialize users
+    // initialize users
 
-  console.log('Done seeding database');
-  await connection.closeConnection();
+    const userCollection = await users();
+
+    const dummy1 = {
+        _id: "nT3Ncadg1iNi4ctaVRKUtE8ki9I3",
+        name: "First dummy",
+        username: "dummy1",
+        email: "dummy1@email.com",
+        goals: [],
+        todo: [],
+        streak: 1,
+        longest: 1,
+        lastLogin: {
+            year: 2023,
+            month: 4,
+            day: 9
+        },
+        image: null
+    }
+
+    const dummy2 = {
+        _id: "kW3dELafBoUD6y2K8Hb9Zf25xZA2",
+        name: "Second dummy",
+        username: "dummy2",
+        email: "dummy2@email.com",
+        goals: [],
+        todo: [],
+        streak: 1,
+        longest: 1,
+        lastLogin: {
+            year: 2023,
+            month: 4,
+            day: 9
+        },
+        image: null
+    }
+
+    const dummy3 = {
+        _id: "3phglUv6mqfTt9RcGXIOuoY3BZi1",
+        name: "Third dummy",
+        username: "dummy3",
+        email: "dummy3@email.com",
+        goals: [],
+        todo: [],
+        streak: 1,
+        longest: 1,
+        lastLogin: {
+            year: 2023,
+            month: 4,
+            day: 9
+        },
+        image: null
+    }
+
+    await userCollection.insertOne(dummy1);
+    await userCollection.insertOne(dummy2);
+    await userCollection.insertOne(dummy3);
+    
+
+    console.log('Done seeding database');
+    await connection.closeConnection();
 };
 
 main().catch(console.log);
