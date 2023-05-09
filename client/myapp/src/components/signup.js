7a1cbd4a28260d1a3649e7ccd092eb4a96141f72
nromboug <noahrombough@gmail.com>
2023-05-09 15:28:54 -0400

    signup validation

--- client/myapp/src/components/signup.js
+++ client/myapp/src/components/signup.js
@@ -18,50 +18,60 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 const Signup = () => {
     const navigate = useNavigate();
     const [error, setError] = useState('');
-    const [name, setName]=useState('');
-    const [email,setEmail]=useState('');
-    const [password,setPassword]=useState('');
-    const [username, setUsername]=useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
 
     const auth = getAuth();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         console.log("signup");
-    
+
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         const password2 = document.getElementById('password2').value;
         const name = document.getElementById('name').value;
         const username = document.getElementById('username').value;
-    
-        if (password === password2) {
+
+        if(!username || username.length<3|| !(/[A-Za-z0-9]{5,}$/.test(username)) || username.trim()==="" || username.indexOf(" ")>-1)
+            setError('Please provide valid username, letters and numbers only of min length 3.');
+        else if (password && password !== password2)
+            setError('Passwords must match');
+        else if (!password || password.length <6)
+            setError('Please provide password min length 6');
+        else if(!/[A-Z]/.test(password))
+            setError("password needs uppercase letter");
+        else if (!/[a-z]/.test(password))
+            setError("password needs lowercase letter");
+        else if (!/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password))
+            setError("password needs special character");
+        else if (!name || name.length<2 || !(/^[A-Za-z ]*$/.test(name)) || name.trim()==="")
+            setError("Please provide valid name, of length min 2 and letters and spaces only.")
+        else {
             try {
+
+
+                const postURL = "http://localhost:3001/users/signup";
                 const userCredential = await createUserWithEmailAndPassword(auth, email, password);
                 console.log("signed up");
                 const uid = userCredential.user.uid;
                 console.log(uid);
-    
-                const postURL = "http://localhost:3001/users/signup";
-    
-                try {
-                    console.log(await axios.post(postURL, {id: uid, name:name, username:username, email:email}));
-                    console.log("inputted data");
-                } catch (e) {
-                    console.log(e);
-                }
-    
+
+                console.log(await axios.post(postURL, { id: uid, name: name, username: username, email: email }));
+                console.log("inputted data");
+                
+
                 navigate('/');
             } catch (error) {
                 console.log(error);
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                setError(errorMessage);
+                if(error.message)
+                    error = error.message
+                setError(error);
             }
-        } else {
-            throw "Error: Passwords do not match";
         }
-    };    
+    };
 
     return (
         <div>
@@ -85,21 +95,21 @@ const Signup = () => {
                         label="Name"
                         className='text-field'
                     />
-                    <br/>
+                    <br />
                     <TextField
                         required
                         id="username"
                         label="Username"
                         className='text-field'
                     />
-                    <br/>
+                    <br />
                     <TextField
                         required
                         id="email"
                         label="Email"
                         className='text-field'
                     />
-                    <br/>
+                    <br />
                     <TextField
                         type="password"
                         required
@@ -107,7 +117,7 @@ const Signup = () => {
                         label="Password"
                         className='text-field'
                     />
-                    <br/>
+                    <br />
                     <TextField
                         type="password"
                         required
